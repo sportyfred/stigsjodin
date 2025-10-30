2d615ef3a7a70aebf0c5d6e8cd9a534ec638ec81
sportyfred <ivanboring@gmail.com>
2025-10-30 15:46:12 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nTEYLWEXRgmiZ1YVIcnRCnVoueHi6Gnt/edit#gid=1661529950';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyDg3KLnpRbEjHD0Da-SidW12jU-Btr7fqM';
