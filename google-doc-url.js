48258fe075f107d395208b79e32c1caf122657c1
sportyfred <ivanboring@gmail.com>
2025-11-23 08:31:15 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 //var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PJtk5V7xM8uBS1P0AE7wkRMFnZqLz_1lqAYQOFBMjpc/edit?gid=1661529950#gid=1661529950';
 var googleDocURL = null;
 // insert your own Google Sheets API key from https://console.developers.google.com
-//var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+//var googleApiKey = '';
